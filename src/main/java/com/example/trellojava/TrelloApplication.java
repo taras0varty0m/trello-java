dbdf2c391009b28d6cebf5d388594b32a1c949a4
Artyom Tarasov <tarasovar457@gmail.com>
2022-06-11 00:38:21 +0600

    build: rename app

--- src/main/java/com/example/trellojava/TrelloApplication.java
+++ src/main/java/com/example/trellojava/TrelloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrelloJavaApplication {
+public class TrelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TrelloJavaApplication.class, args);
+        SpringApplication.run(TrelloApplication.class, args);
     }
 
 
