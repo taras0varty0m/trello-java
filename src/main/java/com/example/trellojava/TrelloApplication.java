398a8f6d4ea12a76d44388c6872f4fab4a875535
Artyom Tarasov <tarasovar457@gmail.com>
2022-06-11 00:33:41 +0600

    Initial commit

---
+++ src/main/java/com/example/trellojava/TrelloApplication.java
@@ -0,0 +1,14 @@
+package com.example.trellojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloJavaApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/trellojava/TrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trellojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
